fa818cfba8d6100387d4c10609020ed58c5dacd3
Shriyansh7870 <Shriyansh7870@gmail.com>
2023-09-14 11:59:16 +0530

    first

--- src/Compo/Pure.js
+++ src/Compo/Pure.js
@@ -15,8 +15,28 @@ export class Pure extends PureComponent {
   render() {
     return (
       <div>
-        <h1>PureComponent</h1>
-        <h2 className="heading2">{this.state.name}</h2>
+        <h1 className="heading">PureComponent</h1>
+        <h2>Explain The Pure Componet in React with Real Example?</h2>
+        <p>
+          Ans:In React, a PureComponent is a class component that inherits from
+          React's Component class but comes with optimizations for<br></br>
+          performance. A PureComponent automatically implements the
+          shouldComponentUpdate method with a shallow comparison of the
+          component's props and state. If the props or state haven't changed, a
+          PureComponent prevents unnecessary re-renders, improving performance
+          in certain situations.
+        </p>
+        <p>Example</p>
+        <p>
+          When you initially render the Pure component, it displays "WELCOME" as
+          the text. Clicking the "Update Name" button <br></br>will change the
+          displayed text to "PrepByte" by updating the name state. Importantly,
+          the Pure component leverages PureComponent to optimize rendering,
+          <br></br> ensuring that unnecessary re-renders are avoided when the
+          state hasn't changed, thereby improving performance in React
+          applications.
+        </p>
+        <h2 className=" ">{this.state.name}</h2>
         <p>
           <button onClick={() => this.updateData()}>Update</button>
         </p>
--- src/Compo/Shriyansh.js
+++ src/Compo/Shriyansh.js
@@ -13,6 +13,21 @@ class Shriyansh extends Component {
     return (
       <div>
         <h1 className="heading">HOC EXAMPLE</h1>
+        <h2>Expalin Hoc With Real Example?</h2>
+        <p>
+          Ans:In React, a Higher-Order Component HOC is a design pattern used
+          for reusing component logic. It's not a built-in component like div or
+          button, but rather a function that takes a component and returns a new
+          component with some enhanced functionality. HOCs are a way to share
+          behavior among multiple components without duplicating code.
+        </p>
+        <p>Example:</p>
+        <p>
+          In this code we have implemented the hoc component in which I haave
+          made the on hover where <br></br> when we hover on the the shriyansh
+          Marks it increase the count . The Mern made in Hoc Component and use
+          in different Component where we use.
+        </p>
         <h2 onMouseOver={this.marksIncrement}>
           Shriyansh's marks:{this.state.marks},{this.props.hocsub}
         </h2>
